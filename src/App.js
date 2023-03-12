a7cff101532fd9c220fc040a892adc33f1de9205
Guy Yomtovian <guyym@mta.ac.il>
2023-03-12 19:59:57 +0200

    demo graph

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale, // x axis 
+  LinearScale, // y axis
+  PointElement
+}from 'chart.js'
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+)
 
 function App() {
+  const data = {
+    labels:['4:25', '4:26', '4:27','4:28','4:29'],
+    datasets:[{
+      labels: 'CPU',
+      data:[0.001999600079983605, 0.001999999999999602, 0.002000400080016316, 0.001999600079983605, 0.001999999999999602, 0.0019988007195678613],
+      backgroundColor:'aqua',
+      borderColor:'white'
+    }]
+  }
+  const options = {
+    plugins:{
+      legend:true
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Line
+        data = {data}
+        options = {options} 
+      ></Line>
     </div>
   );
 }
